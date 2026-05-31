902e771956fd2ab8bf074f65be23d5304f0757f9
Smarika <smarikakc96@gmail.com>
2026-05-31 11:06:29 +0545

    Corected using flake8 andautopep8

--- app/models.py
+++ app/models.py
@@ -1,44 +1,43 @@
-from sqlalchemy import Column,String,Integer,DateTime,ForeignKey,Enum
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from database import Base
 from datetime import datetime
 import enum
 
+
 class StatusEnum(enum.Enum):
-    pending="pending"
-    in_progress="in_progress"
-    completed="completed"
+    pending = "pending"
+    in_progress = "in_progress"
+    completed = "completed"
 
 
 class User(Base):
-    __tablename__="user"
-    id=Column(Integer,primary_key=True)
-    username=Column(String,nullable=False,unique=True)
-    hashed_password=Column(String,nullable=False)
-    email=Column(String,nullable=False,unique=True)
-    created_at=Column(DateTime,default=datetime.now)
-    lists=relationship("Lists",back_populates="user")
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    email = Column(String, nullable=False, unique=True)
+    created_at = Column(DateTime, default=datetime.now)
+    lists = relationship("Lists", back_populates="user")
+
 
 class Lists(Base):
-    __tablename__="lists"
-    id=Column(Integer,primary_key=True)
-    name=Column(String,nullable=False)
-    user_id=Column(Integer,ForeignKey("user.id"))
-    created_at=Column(DateTime,default=datetime.now)
-    user=relationship("User",back_populates="lists")
-    tasks=relationship("Tasks",back_populates="lists")
-
-
+    __tablename__ = "lists"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    created_at = Column(DateTime, default=datetime.now)
+    user = relationship("User", back_populates="lists")
+    tasks = relationship("Tasks", back_populates="lists")
 
 
 class Tasks(Base):
-    __tablename__="tasks"
-    id=Column(Integer,primary_key=True)
-    title=Column(String,nullable=False)
-    description=Column(String)
-    status=Column(Enum(StatusEnum),default=StatusEnum.pending)
-    created_at=Column(DateTime,default=datetime.now)
-    due_date=Column(DateTime)
-    list_id=Column(Integer,ForeignKey("lists.id"))
-    lists=relationship("Lists",back_populates="tasks")
-
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    description = Column(String)
+    status = Column(Enum(StatusEnum), default=StatusEnum.pending)
+    created_at = Column(DateTime, default=datetime.now)
+    due_date = Column(DateTime)
+    list_id = Column(Integer, ForeignKey("lists.id"))
+    lists = relationship("Lists", back_populates="tasks")
